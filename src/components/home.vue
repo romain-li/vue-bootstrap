33a33f9824d95e1816cc9348e963b788794766a0
tangjingjing <1587554777@qq.com>
2016-12-11 13:39:34 +0800

    **

--- src/components/home.vue
+++ src/components/home.vue
@@ -4,7 +4,7 @@
       <slide :msg.sync="aa"></slide>  <!--.sync双向绑定-->
       <span>测试父子双向绑定：{{aa}}</span>
       <div class="home-list">
-        <h2 class="home-title">我是薛之谦，我的梦想是世界和平...</h2>
+        <h2 class="home-title">我是薛之谦，我的梦想是世界和平....</h2>
         <p class="home-detail">我是薛之谦，我的梦想是世界和平...我是薛之谦，我的梦想是世界和平...我是薛之谦，我的梦想是世界和平...我是薛之谦，我的梦想是世界和平...我是薛之谦，我的梦想是世界和平...</p>
       </div>
       <div class="home-list">
